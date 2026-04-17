00c528485f6c19a1244f3dcf5d9a70cdbd81439a
noemi bonilla <noemibonilla95@hotmail.com>
2026-04-17 20:40:11 +0200

    creating environment's react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 
+import { BrowserRouter, Route, Routes } from "react-router"
+
+
+
+import AddNewTrade from "./pages/AddNewTrade"
+import Dashboard from "./pages/Dashboard"
+import Settings from "./pages/Statics"
+import Trades from "./pages/Trades"
 
 function App() {
 
-  return (
-    <>
-    <h1 className="text-xl">hola esta es mi app</h1>
-    </>
-  )
+	return (
+			<BrowserRouter>
+				<Routes>
+					<Route path="/addtrade" element={<AddNewTrade />} />
+					<Route path="/" element={<Dashboard />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/alltrades" element={<Trades />} />
+				</Routes>
+			</BrowserRouter>
+	)
 }
 
 export default App
