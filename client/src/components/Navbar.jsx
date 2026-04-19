7d5fb50cdaab40865f4fd4eb61c40fa3c43a7163
noemi bonilla <noemibonilla95@hotmail.com>
2026-04-19 12:42:30 +0200

    stayled navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,12 +4,22 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
- <nav className="">
-    <Link to={"/settings"}>configuracion</Link>
-    <Link to={"/"}>inicio</Link>
-    <Link to={"/alltrades"}>trades</Link>
-    <Link to={"/addtrade"}>nuevo trade</Link>
- </nav>
+    <nav className="flex justify-between font-bold capitalize ">
+      <Link to={"/"}>inicio</Link>
+      <Link to={"/alltrades"}>trades</Link>
+
+      <div>
+        {/**el boton de agrgar un nuevo trade se muestra solo cuando se ha iniciado secion */}
+        <Link to={"/addtrade"}>nuevo trade</Link>
+      </div>
+      
+      <div>
+        <button c>inicio</button>
+        {/* boton inicio secion o crear una cuenta*/}
+        {/**el boton de configuracion se muestra solo cuando se ha iniciado secion */}
+        <Link to={"/settings"}>configuracion</Link>
+      </div>
+    </nav>
   )
 }
 
