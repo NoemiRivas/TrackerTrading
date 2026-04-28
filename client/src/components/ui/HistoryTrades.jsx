a04e78fd0151a3ffffa9bb106e445e942d8ab14f
noemi bonilla <noemibonilla95@hotmail.com>
2026-04-28 06:01:37 +0200

    crating the component  trade's history and table: layout whit taillwind

--- client/src/components/ui/HistoryTrades.jsx
+++ client/src/components/ui/HistoryTrades.jsx
@@ -3,13 +3,36 @@ import mockData from '../../utils/mockData'
 
 function HistoryTrades() {
 
- console.log(mockData.mockTrades)
   return (
     <div>
-      HistoryTrades
-      {
-        /**creating a map of trades */
-      }
+      <h1 className='text-left text-2xl mb-2'>ultimas operaciones</h1>
+
+      <div >
+        <table className='w-full capitalize text-sm rounded-4xl'>
+          <thead className='bg-tablethead' >
+            <tr>
+              <th>numero</th>
+              <th>fecha</th>
+              <th>instrumento</th>
+              <th>setup</th>
+              <th>resultado</th>
+              <th>pnlNeto</th>
+            </tr>
+          </thead>
+          <tbody >
+            {mockData.mockTrades.map(({ id, fecha, instrumento, setup, resultado, pnlNeto }, index) => (
+              <tr key={index}>
+                <th>{id}</th>
+                <th>{fecha}</th>
+                <th>{instrumento}</th>
+                <th>{setup}</th>
+                <th>{resultado}</th>
+                <th>{pnlNeto}</th>
+              </tr>))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   )
 }
